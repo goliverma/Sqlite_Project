d2f1aaf820d415f79ea1cf3cbedb940df0eeaaa3
Attitude <goli@pop-os.localdomain>
2020-07-19 18:06:25 +0530

    Project is ready to use we make a succesful connection with sqlite database project is run with out error.

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Project.Model.Repository.Interface;
 
 namespace Project.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IBAL context;
+
+        public HomeController(IBAL context)
         {
-            return View();
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var data = await context.GetAllEmployee();
+            return View(data);
         }
     }
 }
