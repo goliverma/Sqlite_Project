960adc94a13e0f6f87305f2f0bd57573a1555140
Attitude <goli@pop-os.localdomain>
2020-07-19 17:53:31 +0530

    this setting is use for dependency injection.

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project.Model.Repository.Interface;
+using Project.Model.Repository.Services;
 
 namespace Project
 {
@@ -23,6 +25,8 @@ namespace Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IDAL, DAL>();
+            services.AddTransient<IBAL, BAL>();
             services.AddControllersWithViews();
         }
 
